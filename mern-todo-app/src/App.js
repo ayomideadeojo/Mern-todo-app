9ea86be2d09972a5444a324f56200044ebbb2f7e
ayomideadeojo <aadeojo@outlook.com>
2019-11-24 15:24:23 -0800

    First Commit

---
+++ mern-todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "boostrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+     <div className="container ">
+      <h2>MERN-STACK Todo</h2>
+    </div>
+    );
+  }
+}
+
+export default App;
