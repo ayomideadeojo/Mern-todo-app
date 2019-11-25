80f3aa1e80bed7dedd2a462a3ee5953781a64854
ayomideadeojo <aadeojo@outlook.com>
2019-11-24 16:12:09 -0800

    Route Path additions

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import "boostrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router,Router,Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
+     <Router>
      <div className="container ">
       <h2>MERN-STACK Todo</h2>
     </div>
+    <Route path="/" exact component={TodosList} />
+    <Route path="/edit/:id" component={EditTodo} />
+    <Route path="/create" component={CreateTodo} />
+
+    </Router>
     );
   }
 }
